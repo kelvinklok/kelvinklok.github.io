1fa679bc2e549c23029b3e3c390744b14da0adac
e5yvx <lokk90@gmail.com>
2021-04-13 16:19:03 -0400

    new lighboxt effect

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,46 @@
-// install image zoom effect
+// install lightbox function on load
 window.onload = function () {
   addEventToFigureImg()
 };
 
+// lightbox
 function addEventToFigureImg() {
-  var htmlFigures = document.getElementsByTagName("img");
+  // add event to all elements
+  var htmlFigures = document.getElementsByTagName("figure");
   
   for (var i = 0; i < htmlFigures.length; i++) {
     htmlFigures[i].addEventListener("click", function() {
+      
+      // toggle zoom and add lightbox effect
       this.classList.toggle("effect-zoom");
+      document.documentElement.classList.add("not-a-modal-background");
+      
+      // center view
+      this.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+      });
+      
+      // count zoom effects
+      var effectCount = document.getElementsByClassName("effect-zoom");
+
+      // if there are no zoom effects
+      if (effectCount.length <= 0) {
+        // then remove lightbox 
+        document.documentElement.classList.remove("not-a-modal-background");
+      }
+
+      // if there are more than 1 effects
+      else if (effectCount.length > 1) {
+        // then find all effects
+        for (var i = 0; i < htmlFigures.length; i++) {
+          // remove zoom
+          htmlFigures[i].classList.remove("effect-zoom");
+        }
+        // toggle zoom
+        this.classList.toggle("effect-zoom");
+      };
+      
     }, false);
   }
 }
